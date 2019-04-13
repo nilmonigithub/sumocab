8803aa41036e65bb3e6c85f610a0589ed7bc2bcd
unknown <pnilmoni.bluehorse@gmail.com>
2019-04-13 11:45:27 +0530

    password bcrypt

--- model/user.model.js
+++ model/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 var userSchema = new mongoose.Schema({
     username: { 
         type: String, 
@@ -23,11 +25,34 @@ var userSchema = new mongoose.Schema({
     createdDate: { type: Date, default: Date.now }
 });
 
- // Custom validation for email
+ // Custom validation for mobile
  userSchema.path('user_mobile').validate((val) => {
     user_mobileRegex =/^([0|+[0-9]{1,5})?([7-9][0-9]{9})$/;
     return user_mobileRegex.test(val);
 }, 'Invalid Mobile No.');
 
+
+//password bcrypt
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('hash')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.hash, salt, function(err, hash) {
+      
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.hash = hash;
+            next();
+        });
+    });
+});
 
 mongoose.model('user', userSchema);
